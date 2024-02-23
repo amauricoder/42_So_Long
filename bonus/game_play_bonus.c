6fd6933953b2d89408f966265a3db12fb47dca2f
Amauri <amauri.olisboa@gmail.com>
2024-02-23 21:23:07 +0000

    functionality of stuck window when enemy get to player and messages

--- bonus/game_play_bonus.c
+++ bonus/game_play_bonus.c
@@ -6,7 +6,7 @@
 /*   By: murilo <murilo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 12:39:09 by murilo            #+#    #+#             */
-/*   Updated: 2024/02/23 20:25:53 by murilo           ###   ########.fr       */
+/*   Updated: 2024/02/23 21:19:37 by murilo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,13 @@ int	game_keypressed_bonus(int keypressed, t_enemy *bonus)
 	p2_keypressed(bonus, keypressed);
 	if (game->exit->exit_x == game->player->pl_x 
 		&& game->exit->exit_y == game->player->pl_y
-		&& game->exit->exit_yes == TRUE)
+		&& game->exit->exit_yes == TRUE && game->player->pl_isonexit == FALSE)
 	{
-		ft_printf("YOU WIN\n");
-		game_close(game);
+		game->player->pl_isonexit = TRUE;
+		ft_printf("PLAYER 1 VICTORYYYYYYYYY\n");
 	}
-	ft_printf("Quantity of moves %i\n", game->qt_mov);
+	if (game->player->pl_isonexit == FALSE)
+		ft_printf("Quantity of moves %i\n", game->qt_mov);
 	ft_printmovs(game);
 	return (0);
 }
@@ -53,19 +54,21 @@ int	p1_keypressed(t_enemy *bonus, int keypressed)
 	t_game *game;
 
 	game = bonus->game_main;
-	if (keypressed == KB_W)
+	if (keypressed == KB_W && game->player->pl_isonexit == FALSE)
 		key_up_pressed(game);
-	else if (keypressed == KB_S)
+	else if (keypressed == KB_S && game->player->pl_isonexit == FALSE)
 		key_down_pressed(game);
-	else if (keypressed == KB_A)
+	else if (keypressed == KB_A && game->player->pl_isonexit == FALSE)
 		key_left_pressed(game);
-	else if (keypressed == KB_D)
+	else if (keypressed == KB_D && game->player->pl_isonexit == FALSE)
 		key_right_pressed(game);
 
 	if (bonus->ht_x == bonus->game_main->player->pl_x
-	&& bonus->ht_y == bonus->game_main->player->pl_y)
+	&& bonus->ht_y == bonus->game_main->player->pl_y
+	&& game->player->pl_isonexit == FALSE)
 	{
-		ft_printf("TOUCH ME BABY\n");
+		game->player->pl_isonexit = TRUE;
+		ft_printf("Player 1 has died\n");
 	}
 	return (0);
 }
@@ -77,20 +80,24 @@ int	p2_keypressed(t_enemy *bonus, int keypressed)
 
 	game = bonus->game_main;
 	map = game->map->map_skeleton;
-
-	if (keypressed == KB_UP)
+	if (keypressed == KB_UP 
+		&& game->player->pl_isonexit == FALSE)
 		p2key_up_pressed(bonus, game, map);
-	else if (keypressed == KB_DOWN)
+	else if (keypressed == KB_DOWN 
+		&& game->player->pl_isonexit == FALSE)
 		p2key_down_pressed(bonus, game, map);
-	else if (keypressed == KB_LEFT)
+	else if (keypressed == KB_LEFT 
+		&& game->player->pl_isonexit == FALSE)
 		p2key_left_pressed(bonus, game, map);
-	else if (keypressed == KB_RIGHT)
+	else if (keypressed == KB_RIGHT 
+		&& game->player->pl_isonexit == FALSE)
 		p2key_right_pressed(bonus, game, map);
-	
 	if (bonus->ht_x == bonus->game_main->player->pl_x
-	&& bonus->ht_y == bonus->game_main->player->pl_y)
+	&& bonus->ht_y == bonus->game_main->player->pl_y
+	&& game->player->pl_isonexit == FALSE)
 	{
-		ft_printf("TOUCH ME BABY\n");
+		game->player->pl_isonexit = TRUE;
+		ft_printf("Player 1 has died\n");
 	}
 	return (0);
 }
